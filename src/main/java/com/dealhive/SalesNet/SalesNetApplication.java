5760d79b339df48429917ed343b82260954acea7
Daniil <lenchikov.daniil@yandex.ru>
2025-02-23 00:46:05 +0300

    Remove old Main class

--- src/main/java/com/dealhive/SalesNet/SalesNetApplication.java
+++ src/main/java/com/dealhive/SalesNet/SalesNetApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SalesNetApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SalesNetApplication.class, args);
 	}
-
 }
