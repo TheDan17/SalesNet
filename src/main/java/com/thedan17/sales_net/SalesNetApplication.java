04c350114be72b347c900a624523c705b133dc3f
Daniil <lenchikov.daniil@yandex.ru>
2025-02-23 01:32:57 +0300

    Change package naming

---
+++ src/main/java/com/thedan17/sales_net/SalesNetApplication.java
@@ -0,0 +1,11 @@
+package com.thedan17.sales_net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesNetApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SalesNetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thedan17/sales_net/SalesNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedan17.sales_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
