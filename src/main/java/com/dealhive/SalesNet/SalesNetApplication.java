e5e6cf1818bd64f87de86d4ee08f98ab767091af
Daniil <lenchikov.daniil@yandex.ru>
2025-02-23 00:17:41 +0300

    Adding Spring Boot (config from Spring Initializr) - other part

---
+++ src/main/java/com/dealhive/SalesNet/SalesNetApplication.java
@@ -0,0 +1,13 @@
+package com.dealhive.SalesNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealhive/SalesNet/SalesNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealhive.SalesNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
