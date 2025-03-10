553ce7739131b964ea31c6d60fe6f0916961a2a5
Daniil <lenchikov.daniil@yandex.ru>
2025-03-10 10:21:16 +0300

    fix: SonarCloud issues

--- src/main/java/com/thedan17/salesnet/model/Account.java
+++ src/main/java/com/thedan17/salesnet/model/Account.java
@@ -1,13 +1,7 @@
 package com.thedan17.salesnet.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -19,7 +13,7 @@ import lombok.Data;
 /**
  * Внутренний класс для представления аккаунта пользователя.
  *
- * <p>{@code TODO}: в будущем у каждого {@code Account} будет несколько {@code User}
+ * <p>В будущем у каждого {@code Account} будет несколько {@code User}, поле Type должно быть enum
  */
 @Data
 @Entity
@@ -39,13 +33,14 @@ public class Account {
   // info data
   private String firstName;
   private String secondName;
-  private String type; // TODO it should be not String in future
+  private String type;
   private LocalDateTime createdAt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 
   @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
   private Set<AccGroupLink> members = new HashSet<>();
 
   /** Получение объектов {@code Group} из связей. */
+  @Transient
   public List<Group> getGroups() {
     List<Group> groups = new ArrayList<>();
     for (AccGroupLink member : this.members) {
