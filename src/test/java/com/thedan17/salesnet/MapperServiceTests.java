54f2afb0317a97b975bdc97fa77acd5213a4d6b9
Daniil <lenchikov.daniil@yandex.ru>
2025-04-01 16:15:28 +0300

    chore(tests): create application-test.properties for "test" profile chore(tests): set "test" profile to unit tests build(tests): add h2 library to pom.xml for testing db-dependence tasks ci: add jacoco plugin to pom.xml for sonarcloud.yml (display unit-test coverage) ci: update sonarcloud.yml for correct Maven and JaCoCo configuration

--- src/test/java/com/thedan17/salesnet/MapperServiceTests.java
+++ src/test/java/com/thedan17/salesnet/MapperServiceTests.java
@@ -6,11 +6,13 @@ import com.thedan17.salesnet.util.MapperService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class MapperServiceTests {
   @Autowired MapperService mapperService;
 
--- src/test/java/com/thedan17/salesnet/SalesNetApplicationTests.java
+++ src/test/java/com/thedan17/salesnet/SalesNetApplicationTests.java
@@ -2,8 +2,10 @@ package com.thedan17.salesnet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SalesNetApplicationTests {
 
 	@Test
